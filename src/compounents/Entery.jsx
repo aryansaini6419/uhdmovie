79ac4eb595bedd114a9e31b40b3dd31224f62616
Aryan <aryansaini6419@gmail.com>
2026-06-21 11:34:48 +0530

    21-06

---
+++ src/compounents/Entery.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Entry = () => {
+  return (
+    <div>
+        <header>
+
+            <div className="logo h-[20vh] w-full border-2 p-3 border-red-500 flex">
+                <div className="img flex h-[15vh] w-full  ">
+                     <img className='imgage border-2 rounded-2xl'  src="././src/assets/logo.png" alt="logo" />
+                </div>
+               
+
+            </div>
+
+        </header>
+
+        <main>
+
+<div className="container">
+
+
+
+</div>
+
+
+        </main>
+
+        <footer>
+            
+            
+            </footer>        
+
+
+    </div>
+  )
+}
+
+export default Entry
